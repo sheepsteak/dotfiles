2f3973f089981677109eef2249b9e0bf435eb9fa
Chris Shepherd <chris@chrisshepherd.me>
2020-08-10 11:17:33 +0100

    Update and rename vscodium.sh to vscode.sh

---
+++ install/vscode.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+extensions=(
+  ahmadawais.shades-of-purple
+  dbaeumer.vscode-eslint
+  EditorConfig.EditorConfig
+  esbenp.prettier-vscode
+  GitHub.vscode-pull-request-github
+  ionicabizau.ms-dos-editor-theme
+  jmrog.vscode-nuget-package-manager
+  jtladeiras.vscode-inline-sql
+  karigari.chat
+  kumar-harsh.graphql-for-vscode
+  mauve.terraform
+  mikestead.dotenv
+  ms-azuretools.vscode-docker
+  MS-CEINTL.vscode-language-pack-en-GB
+  ms-dotnettools.csharp
+  ms-vscode.cpptools
+  ms-vscode.sublime-keybindings
+  ms-vsliveshare.vsliveshare
+  ms-vsliveshare.vsliveshare-audio
+  naumovs.color-highlight
+  nopjmp.fairyfloss
+  philippnowinski.complementary
+  redhat.vscode-yaml
+  sdras.night-owl
+  Shan.code-settings-sync
+  teabyii.ayu
+  VisualStudioExptTeam.vscodeintellicode
+  vsls-contrib.spaces
+  ziyasal.vscode-open-in-github
+)
+
+for extension in "${extensions[@]}"
+do
+  vscode --install-extension "$extension"
+done
